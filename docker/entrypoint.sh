81d26235e1265e7e8544bed83358efa691bcb7c0
Filippo Marcon <f.marcon@donboscosandona.it>
2026-06-26 18:12:57 +0200

    entrypoint genera .env e APP_KEY automaticamente

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,12 @@ PORT=${PORT:-80}
 sed -i "s/listen 80;/listen ${PORT};/g" /etc/nginx/http.d/default.conf
 sed -i "s/listen \[::\]:80;/listen [::]:${PORT};/g" /etc/nginx/http.d/default.conf
 
+if [ ! -f /var/www/.env ]; then
+    cp /var/www/.env.example /var/www/.env
+fi
+
+if [ -z "$APP_KEY" ]; then
+    php /var/www/artisan key:generate --force
+fi
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
